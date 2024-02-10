1bca6328c84540cf8d1d39b4d513df01a9682b6c
Emanuel Gustafzon <104900900+EmanuelGustafzon@users.noreply.github.com>
2024-02-10 22:11:03 +0100

    Update PostCardServerAction.tsx
    Change image src

--- app/components/PostCardServerAction.tsx
+++ app/components/PostCardServerAction.tsx
@@ -38,11 +38,10 @@ const PostCard: React.FC<IPostCard> = async ({ searchTerm }) => {
         const h1Content = $('h1').text();
         const pContent = $('p').first().text();
         const imgSrc = $('img').first().attr('src');
-        const img = `/${imgSrc}`;
 
         return (
           <div key={post._id} className="card w-96 glass m-2">
-           <figure><Image src={img || '/images/trolltunga.jpg'} width={390} height={250} alt="car!" /></figure>
+           <figure><Image src={imgSrc} width={390} height={250} alt="car!" /></figure>
             <div className="card-body">
               <h2 className="card-title">{h1Content}</h2>
               <p>{pContent}</p>
