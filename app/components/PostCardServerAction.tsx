c399fad4874a44a9b24ae5747f8c2b2364ad70d1
EmanuelGustafzon <emanuel.gustafzon@gmail.com>
2024-02-10 22:20:05 +0100

    change img src and alt

--- app/components/PostCardServerAction.tsx
+++ app/components/PostCardServerAction.tsx
@@ -38,10 +38,11 @@ const PostCard: React.FC<IPostCard> = async ({ searchTerm }) => {
         const h1Content = $('h1').text();
         const pContent = $('p').first().text();
         const imgSrc = $('img').first().attr('src');
+        const imgAlt = $('img').first().attr('alt');
 
         return (
           <div key={post._id} className="card w-96 glass m-2">
-           <figure><Image src={imgSrc} width={390} height={250} alt="car!" /></figure>
+           <figure><Image src={imgSrc || ""} width={390} height={250} alt={imgAlt || ""} /></figure>
             <div className="card-body">
               <h2 className="card-title">{h1Content}</h2>
               <p>{pContent}</p>
